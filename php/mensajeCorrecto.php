f3eb2a1662ffc8fb008adaf29241127c8a0f30c6
RC <ronnychamba96@gmail.com>
2021-06-02 19:15:52 -0500

    terminado

---
+++ php/mensajeCorrecto.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mensaje</title>
+    <link rel="stylesheet" href="../assets/css/normalize.css">
+    <link rel="stylesheet" href="../assets/css/generales.css" />
+   
+  </head>
+  <body>
+   
+    <div class="container">
+
+      <div class="content content--mensaje">
+      
+      <h1 class="title">Mensaje del Sistema</h1>
+      <div class="mensaje">
+        <img
+          src="../assets/img/visto2.png"
+          alt="Operación Realizada con exito"
+          title="icono mensaje"
+          />
+        <p>La operación se realizo con exito!.</p>
+        <a href="">Volver al inicio</a>
+      </div>
+   
+      </div>
+
+    </div>
+
+   
+  </body>
+</html>
